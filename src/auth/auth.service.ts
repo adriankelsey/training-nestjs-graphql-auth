714cf8bbc16ce4a26085ecb9a09a816c734f0e71
Adrian Kelsey <adriankelsey@Adrians-MacBook-Pro.local>
2022-01-11 11:22:31 +0000

    auth module setup

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+    constructor(private usersService: UsersService){}
+
+    async validateUser(username: string, passowrd: string): Promise<any> {
+        const user = await this.usersService.findOne(username);
+
+        if (user && user.password === passowrd) {
+            // destructure passowrd out of obect so it is secure when returning the object
+            const { password, ...result } = user;
+            return result
+        }
+
+        return null;
+    }
+}
